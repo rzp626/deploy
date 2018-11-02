5d807580f85130e9e14b5d216708926ffd4f4e2d
rzp <php_net@163.com>
2018-11-02 18:06:57 +0800

    add vendor

---
+++ vendor/andres-montanez/magallanes/src/Task/BuiltIn/Symfony/AssetsInstallTask.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * This file is part of the Magallanes package.
+ *
+ * (c) Andrés Montañez <andres@andresmontanez.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mage\Task\BuiltIn\Symfony;
+
+use Symfony\Component\Process\Process;
+
+/**
+ * Symfony Task - Install Assets
+ *
+ * @author Andrés Montañez <andresmontanez@gmail.com>
+ */
+class AssetsInstallTask extends AbstractSymfonyTask
+{
+    public function getName()
+    {
+        return 'symfony/assets-install';
+    }
+
+    public function getDescription()
+    {
+        return '[Symfony] Assets Install';
+    }
+
+    public function execute()
+    {
+        $options = $this->getOptions();
+        $command = sprintf('%s assets:install %s --env=%s %s', $options['console'], $options['target'], $options['env'], $options['flags']);
+
+        /** @var Process $process */
+        $process = $this->runtime->runCommand(trim($command));
+
+        return $process->isSuccessful();
+    }
+
+    protected function getSymfonyOptions()
+    {
+        return ['target' => 'web', 'flags' => '--symlink --relative'];
+    }
+}
